b4b4451f23d0f1f511a86de7c49156b8dd77efa3
tilek075 <tomurbekov@gmail.com>
2023-03-04 10:18:20 +0600

    hw7

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from products.models import Product, Review
 from products.forms import ProductCreateForm, ReviewCreateForm
+from products.constants import PAGINATION_LIMIT
 
 
 def main_page_view(request):
@@ -11,6 +12,19 @@ def main_page_view(request):
 def products_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
+        search = request.GET.get('search')
+        page = int(request.GET.get('page', 1))
+
+        if search:
+            products = products.filter(title__contains=search) | products.filter(description__contains=search)
+
+        max_page = products.__len__() / PAGINATION_LIMIT
+        if round(max_page) < max_page:
+            max_page = round(max_page) + 1
+        else:
+            max_page = round(max_page)
+
+        products = products[PAGINATION_LIMIT * (page-1):PAGINATION_LIMIT * page]
 
         context = {
             'products': [
@@ -22,7 +36,9 @@ def products_view(request):
                     'hashtags': product.hashtags.all()
                 }
                 for product in products
-            ]
+            ],
+            'user': request.user,
+            'pages': range(1, max_page+1)
         }
 
         return render(request, 'products/products.html', context=context)
